1e50a35b4a112a33846ba3e1556a2ce079c88a0c
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-08-26 20:01:29 +0200

    Code normalization.

--- code.js
+++ code.js
@@ -1,14 +1,14 @@
-const statusValue = document.getElementById("statusValue");
-const dropdownDifficulty = document.getElementById("dropdownDifficulty");
-const dropdownDifficultyBtn = document.getElementById("dropdown");
-const red = document.getElementById("red");
-const green = document.getElementById("green");
-const blue = document.getElementById("blue");
-const easyCaller = document.getElementById("levelEasyBtn");
-const mediumCaller = document.getElementById("levelMediumBtn");
-const hardCaller = document.getElementById("levelHardBtn");
-const colors = document.querySelectorAll(".oneOfColors");
-const newColorsBtn = document.getElementById("newColorsBtn");
+const statusValue = document.getElementById('statusValue');
+const dropdownDifficulty = document.getElementById('dropdownDifficulty');
+const dropdownDifficultyBtn = document.getElementById('dropdown');
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+const easyCaller = document.getElementById('levelEasyBtn');
+const mediumCaller = document.getElementById('levelMediumBtn');
+const hardCaller = document.getElementById('levelHardBtn');
+const colors = document.querySelectorAll('.oneOfColors');
+const newColorsBtn = document.getElementById('newColorsBtn');
 let correctColorSquare;
 let maxDependDifficulty = 9;
 let correctRed;
@@ -16,7 +16,7 @@ let correctGreen;
 let correctBlue;
 let tryNums = 1;
 resizeSquares();
-dropdownDifficulty.textContent = "HARD";
+dropdownDifficulty.textContent = 'HARD';
 
 function randomColorSyntax() {
   return Math.floor(Math.random() * 256);
@@ -27,49 +27,49 @@ function correctSquare() {
 }
 
 function easyDifficulty() {
-  document.getElementById("lvlMedium").style.visibility = "hidden";
-  document.getElementById("lvlHard").style.visibility = "hidden";
+  document.getElementById('lvlMedium').style.visibility = 'hidden';
+  document.getElementById('lvlHard').style.visibility = 'hidden';
   maxDependDifficulty = 3;
-  dropdownDifficulty.textContent = "EASY";
-  dropdownDifficultyBtn.classList.remove("btn-danger");
-  dropdownDifficultyBtn.classList.remove("btn-warning");
-  dropdownDifficultyBtn.classList.add("btn-success");
-  easyCaller.classList.add("levelEasyBtnActive");
-  mediumCaller.classList.remove("levelMediumBtnActive");
-  hardCaller.classList.remove("levelHardBtnActive");
+  dropdownDifficulty.textContent = 'EASY';
+  dropdownDifficultyBtn.classList.remove('btn-danger');
+  dropdownDifficultyBtn.classList.remove('btn-warning');
+  dropdownDifficultyBtn.classList.add('btn-success');
+  easyCaller.classList.add('levelEasyBtnActive');
+  mediumCaller.classList.remove('levelMediumBtnActive');
+  hardCaller.classList.remove('levelHardBtnActive');
   newColor();
 }
 
 function mediumDifficulty() {
-  document.getElementById("lvlMedium").style.visibility = "visible";
-  document.getElementById("lvlHard").style.visibility = "hidden";
+  document.getElementById('lvlMedium').style.visibility = 'visible';
+  document.getElementById('lvlHard').style.visibility = 'hidden';
   maxDependDifficulty = 6;
-  dropdownDifficulty.textContent = "MEDIUM";
-  dropdownDifficultyBtn.classList.remove("btn-danger");
-  dropdownDifficultyBtn.classList.add("btn-warning");
-  dropdownDifficultyBtn.classList.remove("btn-success");
-  easyCaller.classList.remove("levelEasyBtnActive");
-  mediumCaller.classList.add("levelMediumBtnActive");
-  hardCaller.classList.remove("levelHardBtnActive");
+  dropdownDifficulty.textContent = 'MEDIUM';
+  dropdownDifficultyBtn.classList.remove('btn-danger');
+  dropdownDifficultyBtn.classList.add('btn-warning');
+  dropdownDifficultyBtn.classList.remove('btn-success');
+  easyCaller.classList.remove('levelEasyBtnActive');
+  mediumCaller.classList.add('levelMediumBtnActive');
+  hardCaller.classList.remove('levelHardBtnActive');
   newColor();
 }
 
 function hardDifficulty() {
-  document.getElementById("lvlMedium").style.visibility = "visible";
-  document.getElementById("lvlHard").style.visibility = "visible";
+  document.getElementById('lvlMedium').style.visibility = 'visible';
+  document.getElementById('lvlHard').style.visibility = 'visible';
   maxDependDifficulty = 9;
-  dropdownDifficulty.textContent = "HARD";
-  dropdownDifficultyBtn.classList.add("btn-danger");
-  dropdownDifficultyBtn.classList.remove("btn-warning");
-  dropdownDifficultyBtn.classList.remove("btn-success");
-  easyCaller.classList.remove("levelEasyBtnActive");
-  mediumCaller.classList.remove("levelMediumBtnActive");
-  hardCaller.classList.add("levelHardBtnActive");
+  dropdownDifficulty.textContent = 'HARD';
+  dropdownDifficultyBtn.classList.add('btn-danger');
+  dropdownDifficultyBtn.classList.remove('btn-warning');
+  dropdownDifficultyBtn.classList.remove('btn-success');
+  easyCaller.classList.remove('levelEasyBtnActive');
+  mediumCaller.classList.remove('levelMediumBtnActive');
+  hardCaller.classList.add('levelHardBtnActive');
   newColor();
 }
-easyCaller.classList.remove("levelEasyBtnActive");
-mediumCaller.classList.remove("levelMediumBtnActive");
-hardCaller.classList.add("levelHardBtnActive");
+easyCaller.classList.remove('levelEasyBtnActive');
+mediumCaller.classList.remove('levelMediumBtnActive');
+hardCaller.classList.add('levelHardBtnActive');
 
 function newColor() {
   tryNums = 1;
@@ -80,55 +80,53 @@ function newColor() {
   red.textContent = correctRed;
   green.textContent = correctGreen;
   blue.textContent = correctBlue;
-  statusValue.textContent = "Ready";
-  newColorsBtn.textContent = "New colors";
+  statusValue.textContent = 'Ready';
+  newColorsBtn.textContent = 'New colors';
   for (let i = 0; i <= (colors.length - 1); i++) {
-    colors[i].style.backgroundColor = "rgb(" + randomColorSyntax() + ", " + randomColorSyntax() + ", " + randomColorSyntax() + ")";
-    colors[i].classList.remove("fade-out");
+    colors[i].style.backgroundColor = `rgb(${randomColorSyntax()}, ${randomColorSyntax()}, ${randomColorSyntax()})`;
+    colors[i].classList.remove('fade-out');
     if (i < maxDependDifficulty) {
-      colors[i].style.visibility = "visible";
-      colors[i].setAttribute("onclick", "checkHit(this, this.id)");
-      colors[i].style.cursor = "pointer";
-
-    } else
-      colors[i].style.visibility = "hidden";
+      colors[i].style.visibility = 'visible';
+      colors[i].setAttribute('onclick', 'checkHit(this, this.id)');
+      colors[i].style.cursor = 'pointer';
+    } else { colors[i].style.visibility = 'hidden'; }
   }
-  colors[correctColorSquare].style.backgroundColor = "rgb(" + correctRed + ", " + correctGreen + ", " + correctBlue + ")";
+  colors[correctColorSquare].style.backgroundColor = `rgb(${correctRed}, ${correctGreen}, ${correctBlue})`;
   if (maxDependDifficulty == 9) {
-    dropdownDifficulty.textContent = "HARD";
+    dropdownDifficulty.textContent = 'HARD';
   } else if (maxDependDifficulty == 6) {
-    dropdownDifficulty.textContent = "MEDIUM";
+    dropdownDifficulty.textContent = 'MEDIUM';
   } else {
-    dropdownDifficulty.textContent = "EASY";
+    dropdownDifficulty.textContent = 'EASY';
   }
 }
 
 function checkHit(target, targetID) {
-  if (targetID === "color" + (correctColorSquare + 1)) {
-    statusValue.textContent = "Correct! Number of tries: " + tryNums;
-    newColorsBtn.textContent = "Play again?";
+  if (targetID === `color${correctColorSquare + 1}`) {
+    statusValue.textContent = `Correct! Number of tries: ${tryNums}`;
+    newColorsBtn.textContent = 'Play again?';
     for (let i = 0; i <= (maxDependDifficulty - 1); i++) {
-      colors[i].style.visibility = "visible";
-      colors[i].setAttribute("onclick", "");
-      colors[i].style.backgroundColor = "rgb(" + correctRed + ", " + correctGreen + ", " + correctBlue + ")";
-      colors[i].style.cursor = "context-menu";
+      colors[i].style.visibility = 'visible';
+      colors[i].setAttribute('onclick', '');
+      colors[i].style.backgroundColor = `rgb(${correctRed}, ${correctGreen}, ${correctBlue})`;
+      colors[i].style.cursor = 'context-menu';
     }
   } else {
-    setTimeout(function() {
-      target.style.visibility = "hidden";
+    setTimeout(() => {
+      target.style.visibility = 'hidden';
     }, 250);
-    target.classList.add("fade-out");
+    target.classList.add('fade-out');
     tryNums++;
   }
 }
 console.log(navigator.userAgent);
-window.addEventListener("resize", resizeSquares);
+window.addEventListener('resize', resizeSquares);
 
 function resizeSquares() {
   // if(window.innerWidth<window.innerHeight) {
   //   console.log("You are on mobile");
   // }
   for (let i = 0; i <= (maxDependDifficulty - 1); i++) {
-    colors[i].style.height = colors[i].offsetWidth + "px";
+    colors[i].style.height = `${colors[i].offsetWidth}px`;
   }
 }
